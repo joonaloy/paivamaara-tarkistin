2809edd53bfa98e94c161c3ccb5513b111381ef8
joona <118251072+joonaloy@users.noreply.github.com>
2023-08-07 12:06:43 +0300

    Add files via upload

---
+++ paivamaara.py
@@ -0,0 +1,53 @@
+#def pvm(p):
+#    pm = list(p)
+#    pm.insert(2," ")
+#    pm.insert(5," ")
+#    return "".join(pm)
+#print(pvm("14062006"))
+def pvm(p):
+    pv=""
+    k=""
+    v=""
+    kv=0
+    pv+=p[0]+p[1]
+    k+=p[2]+p[3]
+    v+=p[4]+p[5]+p[6]+p[7]
+    pv=int(pv)
+    k=int(k)
+    v=int(v)
+
+    if v%400==0 and v%100==0:
+        kv=1
+    elif v%4==0 and v%100!=0:
+        kv=1
+    else:
+        kv=0
+    if k==1 or k==3 or k==5 or k==7 or k==8 or k==10 or k==12:
+        if pv<32:
+            print(pv,k,v,"On oikea päivämäärä")
+        else:
+            print(pv,k,v,"Ei ole oikea päivämäärä")
+    if k==4 or k==6 or k==9 or k==11:
+        if pv<31:
+            print(pv,k,v,"On oikea päivämäärä")
+        else:
+            print(pv,k,v,"Ei ole oikea päivämäärä")
+    if k==2:
+        if kv==1:
+            if pv<30:
+                print(pv,k,v,"On oikea päivämäärä")
+            else:
+                print(pv,k,v,"Ei ole oikea päivämäärä")
+                
+        if kv==0:
+            if pv<29:
+                print(pv,k,v,"On oikea päivämäärä")
+            else:
+                print(pv,k,v,"Ei ole oikea päivämäärä") 
+    if k<1 or k>12:
+        print(pv,k,v,"Ei ole oikea päivämäärä")
+
+pvm("14062006")
+pvm("29022024")
+pvm("29022023")
+pvm("01132020")
