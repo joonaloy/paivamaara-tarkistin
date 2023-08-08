b549c9bb0885f4296ea2ef3b5b2b3cbe106d3ae7
joona <118251072+joonaloy@users.noreply.github.com>
2023-08-08 09:09:09 +0300

    Update paivamaara.py

--- paivamaara.py
+++ paivamaara.py
@@ -1,23 +1,25 @@
 def pvm(p):
-    #erittelee inputin päiviin kuukausiin ja vuosiin
+    #päivä,kk,vuosi muuttujat
     pv=""
     k=""
     v=""
+    #karkausvuosi muuttuja
     kv=0
+    #erittelee inputin päiviin kuukausiin ja vuosiin
     pv+=p[0]+p[1]
     k+=p[2]+p[3]
     v+=p[4]+p[5]+p[6]+p[7]
     pv=int(pv)
     k=int(k)
     v=int(v)
-    #karkausvuosi tarkistus
+    #tarkistaa onko vuosi karkausvuosi
     if v%400==0 and v%100==0:
         kv=1
     elif v%4==0 and v%100!=0:
         kv=1
     else:
         kv=0
-    #tarkistaa onko päivä suurempi kun mahdollista
+    #tarkistaa onko päivä suurempi kuin kuukauden viimeinen päivä ja printtaa vastauksen sen perusteella
     if k==1 or k==3 or k==5 or k==7 or k==8 or k==10 or k==12:
         if pv<32:
             print(pv,k,v,"On oikea päivämäärä")
@@ -40,6 +42,7 @@ def pvm(p):
                 print(pv,k,v,"On oikea päivämäärä")
             else:
                 print(pv,k,v,"Ei ole oikea päivämäärä") 
+    #tarkistaa onko kuukausi suurempi tai pienempi kuin mahdollista
     if k<1 or k>12:
         print(pv,k,v,"Ei ole oikea päivämäärä")
 
